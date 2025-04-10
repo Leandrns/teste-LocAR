80b6a240a513bc844853badef3cc1e667845df2a
Leandro Souza <leandro2005souza@gmail.com>
2025-04-10 19:42:16 -0300

    Adicionando comentários no código

--- src/main.js
+++ src/main.js
@@ -1,95 +1,73 @@
 import * as THREE from "three";
 import * as LocAR from "locar";
 
+// Configurando a câmera
 const camera = new THREE.PerspectiveCamera(
 	80,
 	window.innerWidth / window.innerHeight,
 	0.001,
 	1000
+	// Mais detalhes sobre esse tipo de câmera em: https://threejs.org/docs/?q=pers#api/en/cameras/PerspectiveCamera
 );
 
+// Criando um renderizador WebGL que exibe as cenas
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+// Mais detalhes: https://threejs.org/docs/?q=web#api/en/renderers/WebGLRenderer
 
+// Criando uma cena
 const scene = new THREE.Scene();
 
+// Criando um AR baseado em localização que recebe a cena e a câmera como parâmetros
 const locar = new LocAR.LocationBased(scene, camera);
 
-window.addEventListener("resize", (e) => {
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+// Ajuste e redimensionamento da câmera de acordo com a janela do navegador
+window.addEventListener("resize", (e) => {	// pega o evento de resize
+	renderer.setSize(window.innerWidth, window.innerHeight); // atualiza o tamanho do renderizador
+	camera.aspect = window.innerWidth / window.innerHeight; // atualiza a proporção da câmera
+	camera.updateProjectionMatrix(); // atualiza a matriz de projeção da câmera
 });
 
+// Criação de um objeto responsável por mostrar o vídeo da câmera como fundo da cena, recebendo o renderizador como parâmetro
 const cam = new LocAR.WebcamRenderer(renderer);
 
 let firstLocation = true;
 
+// Criação de um objeto para controlar a orientação do dispositivo, recebendo a câmera como parâmetro
 const deviceOrientationControls = new LocAR.DeviceOrientationControls(camera);
 
+// Adicionando um evento manipulador, que nesse ocorre quando o GPS do dispositivo é atualizado
 locar.on("gpsupdate", (pos, distMoved) => {
 	if (firstLocation) {
-		// const boxProps = [
-		// 	{
-		// 		latDis: 0.0009,
-		// 		lonDis: 0,
-		// 		colour: 0xff0000,
-		// 	},
-		// 	{
-		// 		latDis: -0.0009,
-		// 		lonDis: 0,
-		// 		colour: 0xffff00,
-		// 	},
-		// 	{
-		// 		latDis: 0,
-		// 		lonDis: -0.0009,
-		// 		colour: 0x00ffff,
-		// 	},
-		// 	{
-		// 		latDis: 0,
-		// 		lonDis: 0.0009,
-		// 		colour: 0x00ff00,
-		// 	},
-		// ];
-
+		// Criando um objeto Three.js que gera uma geometria de cubo
 		const geom = new THREE.BoxGeometry(20, 20, 20);
 
-		// for (const boxProp of boxProps) {
-		// 	const mesh = new THREE.Mesh(
-		// 		geom,
-		// 		new THREE.MeshBasicMaterial({ color: boxProp.colour })
-		// 	);
-
-		// 	locar.add(
-		// 		mesh,
-		// 		pos.coords.longitude + boxProp.lonDis,
-		// 		pos.coords.latitude + boxProp.latDis
-		// 	);
-
-		// }
-
+		// Criando uma malha 3D passando a geometria e o material do objeto como parâmetros
 		const mesh = new THREE.Mesh(
 			geom,
-			new THREE.MeshPhongMaterial({ color: "black", emissive: "purple", shininess: 10 })
+			new THREE.MeshPhongMaterial({ color: "black", emissive: "purple", shininess: 100 })
+			// O material é um MeshPhongMaterial, que é um material que reflete a luz
+			// Mais detalhes sobre os materiais podem ser vistos em https://threejs.org/manual/#en/materials
 		);
 
+		// Adicionando o objeto 3D em uma latitude e longitude específicas
 		locar.add(
 			mesh,
-			pos.coords.longitude + 0.0009,
-			pos.coords.latitude + 0.0009
+			pos.coords.longitude + 0.0009, // pega a longitude do GPS e adiciona 0.0009
+			pos.coords.latitude + 0.0009 // pega a latitude do GPS e adiciona 0.0009
 		);
         
-		firstLocation = false;
+		firstLocation = false; // para não criar mais de um objeto
 	}
 });
 
-locar.startGps();
+locar.startGps(); // Inicia o GPS
 
-renderer.setAnimationLoop(animate);
+renderer.setAnimationLoop(animate); // Executa a geração do objeto 3D em loop
 
 function animate() {
-	cam.update();
-	deviceOrientationControls.update();
-	renderer.render(scene, camera);
+	cam.update(); // Atualiza o vídeo da câmera
+	deviceOrientationControls.update(); // Atualiza a orientação da câmera com base nos movimentos do dispositivo
+	renderer.render(scene, camera); // Renderiza a cena e a câmera atualizados
 }
